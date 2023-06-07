51b14eb832ea113eb3905c8b4e1de259854c1935
Ilya547 <Ilya547@bk.ru>
2023-06-07 14:21:50 +0300

    Initial commit

---
+++ src/main/java/org/example/domain/Message.java
@@ -0,0 +1,47 @@
+package org.example.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String text;
+    private String tag;
+
+    public Message(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public Message() {
+
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
