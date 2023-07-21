af86caa172607c0b59147e63e4a578729f4cc257
Ilya547 <Ilya547@bk.ru>
2023-07-21 10:05:25 +0300

    fix security in memory

--- src/main/java/org/example/config/WebSecurityConfig.java
+++ src/main/java/org/example/config/WebSecurityConfig.java
@@ -1,17 +1,20 @@
 package org.example.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+//import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.*;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
@@ -19,7 +22,6 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
     @Autowired
     private DataSource dataSource;
 
@@ -39,19 +41,20 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
 
+//     only in memory
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
         return new InMemoryUserDetailsManager(user);
     }
-//    @Bean
-//    protected UserDetailsService configure(AuthenticationManagerBuilder auth) throws Exception {
+
+//    @Autowired
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //        auth.jdbcAuthentication()
 //                .dataSource(dataSource)
 //                .passwordEncoder(NoOpPasswordEncoder.getInstance())
@@ -59,5 +62,4 @@ public class WebSecurityConfig {
 //                .authoritiesByUsernameQuery("select u.userName, ur.roles from usr u inner join user_role ur on u.id =" +
 //                        " ur.user_id where u.userName=?");
 //    }
-
 }
